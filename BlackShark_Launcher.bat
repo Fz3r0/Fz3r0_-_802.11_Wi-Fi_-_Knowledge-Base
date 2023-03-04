e2820a24d48b1f93cda5b16823412926b731d76d
Fz3r0 <94720207+Fz3r0@users.noreply.github.com>
2023-03-04 10:30:42 -0500

    Create BlackShark_Launcher.bat

---
+++ BlackShark_Launcher.bat
@@ -0,0 +1,28 @@
+@echo off
+REM Este es un comentario
+
+echo """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+echo "                                                                                          "
+echo "         @@@@@@@@@@@@@@@@@@             ((_.-'-._| BlackShark v3.6 by Fz3r0 |_.-'-._))    "
+echo "       @@@@@@@@@@@@@@@@@@@@@@@                                                            "
+echo "     @@@@@@@@@@@@@@@@@@@@@@@@@@@        Networking & Cyber-Security PCAP Analysis Tool    "
+echo "    @@@@@@@@@@@@@@@@@@@@@@@@@@@@@                                                         "
+echo "   @@@@@@@@@@@@@@@/      \@@@/   @      [+] Cyber-Weapon:............. BlackShark         "
+echo "  @@@@@@@@@@@@@@@@\      @@  @___@      [+] Version:.................. 3.6                "
+echo "  @@@@@@@@@@@@@ @@@@@@@@@@  | \@@@@@    [+] Author:................... Fz3r0              "
+echo "  @@@@@@@@@@@@@ @@@@@@@@@\__@_/@@@@@    [+] Github:................... github.com/Fz3r0   "
+echo "   @@@@@@@@@@@@@@@/,/,/./'/_|.\'\,\     [+] Twitter:.................. @Fz3r0_OPs         "
+echo "     @@@@@@@@@@@@@|  | | | | | | | |    [+] Youtube:.................. @Fz3r0_OPs         "
+echo "                 \_|_|_|_|_|_|_|_|                                                        "
+echo "                                                                                          "
+echo "                                                                                          "
+echo """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+echo           Bienvenido a BlackShark!!! Presiona cuaqluier tecla para continuar...
+pause > nul
+echo Ejecutando BlackShark, espere un momento...
+echo.
+start "" /D "C:\Program Files\Wireshark" "Wireshark.exe" -platform windows:darkmode=2
+echo.
+echo BlackShark ejecutado!!! 
+pause > nul
