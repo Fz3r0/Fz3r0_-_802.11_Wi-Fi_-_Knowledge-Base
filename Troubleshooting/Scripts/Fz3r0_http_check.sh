ae2956aec5b2095ce1dc614ed95e436f45e59b94
Fz3r0 <94720207+Fz3r0@users.noreply.github.com>
2023-06-26 13:08:23 -0500

    Update Fz3r0_http_check.sh

--- Troubleshooting/Scripts/Fz3r0_http_check.sh
+++ Troubleshooting/Scripts/Fz3r0_http_check.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-# Función para verificar si una dirección IP responde a través de HTTP o HTTPS
+# Función para verificar si una dirección IP responde a través de HTTP
+
+# perfecto para encontrar equipos!!!
+
 check_http_response() {
     ip=$1
-    http_response=$(curl -s -I "http://$ip" -o /dev/null -w "%{http_code}")
-    https_response=$(curl -s -I "https://$ip" -o /dev/null -w "%{http_code}")
-    
-    if [ "$http_response" = "200" ] || [ "$https_response" = "200" ]; then
-        echo "La dirección $ip respondió correctamente a través de HTTP o HTTPS"
+    response=$(curl -s -I "http://$ip" -o /dev/null -w "%{http_code}")
+    if [ "$response" = "200" ]; then
+        echo "La dirección $ip respondió correctamente a través de HTTP"
     fi
 }
 
-# Iterar sobre las direcciones IP y comprobar la respuesta HTTP o HTTPS
-for i in {192..192}; do
-    for j in {168..168}; do
-        for k in {34..34}; do
-            for l in {1..5}; do
-                ip="$i.$j.$k.$l"
-                check_http_response "$ip"
-            done
-        done
+# Rango de direcciones IP
+ranges=("192.168.33" "192.168.34" "192.168.35")
+
+# Iterar sobre los rangos de direcciones IP y comprobar la respuesta HTTP
+for range in "${ranges[@]}"; do
+    for i in {1..254}; do
+        ip="$range.$i"
+        check_http_response "$ip"
     done
 done
