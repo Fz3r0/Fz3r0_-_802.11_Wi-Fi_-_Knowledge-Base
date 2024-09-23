ee55d8480079e43ed9140d45e2a63d8b26d84c82
Fz3r0 <94720207+Fz3r0@users.noreply.github.com>
2024-09-23 09:40:29 -0500

    Update Fz3r0-4-way-handshake-crack.py

--- CWAP/Fz3r0_-_Hacia-la-cima-del-CWAP/4-way-handshake/Fz3r0-4-way-handshake-crack.py
+++ CWAP/Fz3r0_-_Hacia-la-cima-del-CWAP/4-way-handshake/Fz3r0-4-way-handshake-crack.py
@@ -8,22 +8,33 @@ count = 0  # Inicializamos la variable count para contar los mensajes del handsh
 for packet in scapycap:
     if packet.haslayer("EAPOL"):
         raw_data = bytes(raw(packet["EAPOL"])).hex()
+
         # Si es el 1º mensaje del handshake
         if count == 0:
-           # Extraemos anonce y lo pasamos a bin
-           anonce = binascii.a2b_hex(raw_data[26:90])
+
+           # Extraemos anonce y lo pasamos a bin:
+           #anonce = binascii.a2b_hex(raw_data[26:90]) 
+           anonce = binascii.a2b_hex(raw_data[34:98])
+
            # Extraemos mac cli, eliminamos ":" y pasamos a hex
            macCli = binascii.a2b_hex(packet.addr1.replace(":", "").lower())
+
         # Si es el segundo mensaje del handshake
         elif count == 1:
+
            # Extraemos el snonce y lo pasamos a bin
-           snonce = binascii.a2b_hex(raw_data[26:90])
+           #snonce = binascii.a2b_hex(raw_data[26:90])
+           snonce = binascii.a2b_hex(raw_data[34:98])
+
            macAP = binascii.a2b_hex(packet.addr3.replace(":", "").lower())
+
            # Almacenamos el MIC
            captured_mic = raw_data[154:186] 
            #captured_mic = raw_data[192:208]  # Ajusta según la posición del MIC
+
            # Almacenamos en hexadecimal la capa de autenticación EAPOL
            Eapol2frame = raw_data
+
         count += 1
 
 # Datos de AP convertidos a HEX legible:
@@ -34,21 +45,34 @@ anonce_hex = binascii.hexlify(anonce).decode('utf-8')
 snonce_hex = binascii.hexlify(snonce).decode('utf-8')
 macSTA_hex = binascii.hexlify(macCli).decode('utf-8')
 
+
+
 # MIC a formato HEX legible:
 captured_mic_hex = binascii.hexlify(binascii.a2b_hex(captured_mic)).decode('utf-8') if captured_mic else "No MIC found"
 
 # Capa de autenticación EAPOL en HEX
 Eapol2frame = Eapol2frame
 
-print("Datos extraídos de AP:")
+
+# Imprime valores de variables:
 print()
-print("MAC AP:", macAP_hex)
-print("Anonce:", anonce_hex)
+print("## Datos extraídos de AP:")
 print()
-print("Datos extraídos de STA:")
+print("- MAC AP:")
+print(">", macAP_hex)
 print()
-print("MAC STA:", macSTA_hex)
-print("SNonce (Hex):", snonce_hex)
+print("- Anonce:")
+print(">", anonce_hex)
+print()
+print("## Datos extraídos de STA:")
+print()
+print("MAC STA:")
+print(">", macSTA_hex)
+print()
+print("SNonce:")
+print(">", snonce_hex)
+print()
+print("## Datos extraídos de MIC:")
 print()
 print("captured MIC:", captured_mic_hex)
 print("Eapol2frame RAW Packet (Hex):", Eapol2frame)
@@ -58,4 +82,49 @@ if count == 0:
     exit()
 
 
+################################
+
+# Derivación PMK
+
+from pbkdf2 import PBKDF2
+
+password = "facility2018"
+ssid = "Facility"
+
+PMK = PBKDF2(password,ssid, 4096).read(32)
+
+
+# Datos de AP convertidos a HEX legible:
+PMK_hex = binascii.hexlify(PMK).decode('utf-8')
+
+print("- PMK:")
+print(">", PMK_hex)
+
+################################
+
+# Derivación PTK
+
+# Formula :: PTK = PRF512(PMK, "Pairwise key expansion", CONCAT(MAC1|MAC2|Nonce1|Nonce2))
+
+import hmac,hashlib
+
+def customPRF512(pmk,txt,key_data):
+    c = 0
+    block = 64 
+    result = bytes()
+    while c<=((block*8+159)/160):
+        hmacsha1 = hmac.new(pmk,txt+chr(0x00).encode()+key_data+chr(c).encode(),hashlib.sha1)
+        result = result + hmacsha1.digest()
+        c += 1
+    return  result[:block]
+
+#Concatenamos MAC1+MAC2+Nonce1+Nonce
+key_data = min(macAP,macCli) + max(macAP,macCli)+ min(anonce,snonce)+ max(anonce,snonce)
+#String especifica de PRF512
+txt = b"Pairwise key expansion"
+#Generamos PTK
+PTK = customPRF512(PMK,txt,key_data)
+
+print("- PTK:")
+print(">", PTK)
   
